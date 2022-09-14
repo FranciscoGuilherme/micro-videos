51e9b5271888239fef7b610e8b29250bf19a6b2b
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-09-14 10:26:29 -0300

    test(test): :test_tube: implementa os testes para a entidade categoria.

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,7 +2,8 @@ import Category from "./category";
 
 describe('Category Tests', () => {
   test('constructor of category', () => {
-    const category = new Category('Movie');
-    expect(category.name).toEqual('Movie');
+    const category = new Category({ name: 'Movie' });
+    expect(category.props.name).toEqual('Movie');
+    expect(category.props).toBeInstanceOf(Object);
   });
 });
