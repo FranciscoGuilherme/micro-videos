3ebfe506280c7367a482503dfce3b3d49dfcc198
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-09-19 09:27:46 -0300

    test(entity-category): :white_check_mark: implementa mais testes.

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -14,7 +14,21 @@ describe('Category Tests', () => {
     expect(category.props.description).toBe('description');
     expect(category.props.is_active).toBeTruthy();
     expect(category.props.created_at).toBe(created_at);
+    expect(category.props.created_at).toBeInstanceOf(Date);
+    expect(category.props.created_at.getTime()).toBe(created_at.getTime());
     expect(category.props).toBeInstanceOf(Object);
     expect(category.props).toStrictEqual(props);
+  });
+
+  test('constructor of category active as false', () => {
+    const props = {
+      name: 'Movie',
+      description: 'description',
+      is_active: false,
+      created_at: new Date()
+    };
+    const category = new Category(props);
+    expect(category.props.is_active).toBeFalsy();
+    expect(category.props).toMatchObject({ is_active: false });
   });
 });
