d3a5e5523db42a3eca6d410ebed46ea20f4988c4
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2022-09-19 08:58:03 -0300

    test(entity-category): :white_check_mark: complementa testes do construtor.

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,8 +2,19 @@ import Category from "./category";
 
 describe('Category Tests', () => {
   test('constructor of category', () => {
-    const category = new Category({ name: 'Movie' });
-    expect(category.props.name).toEqual('Movie');
+    const created_at = new Date();
+    const props = {
+      name: 'Movie',
+      description: 'description',
+      is_active: true,
+      created_at: created_at
+    };
+    const category = new Category(props);
+    expect(category.props.name).toBe('Movie');
+    expect(category.props.description).toBe('description');
+    expect(category.props.is_active).toBeTruthy();
+    expect(category.props.created_at).toBe(created_at);
     expect(category.props).toBeInstanceOf(Object);
+    expect(category.props).toStrictEqual(props);
   });
 });
